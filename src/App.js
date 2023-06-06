fcc479b784519d51e139df412b0da9f866808c5b
HrishikeshShinde3 <rushishinde0303@gmail.com>
2023-06-06 09:50:44 +0530

    Dummy compo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
+import { useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import './App.css';
-//import AssignmentFunctionality from './component/AssignmentFunctionality';
-import { Editor } from './component/Editor';
-//import AssignmentFunctionality from './component/AssignmentFunctionality';
-//import UndoRedoExample from './component/AssignmentFunctionality';
 function App() {
+  const editorRef = useRef();
   return (
-   <div>
-  
-    <Editor/>
-    
-    
-   
-   </div>
-    
+    <div className="Main-Div">
+         <h3>Description</h3>
+      <div className="Editor-Div">
+        <Editor
+          onInit={(evt, editor) => (editorRef.current = editor)}
+          apiKey="YOUR_TINYMCE_API_KEY"
+        />
+      </div>
+
+
+     
+      <Stack className="buttons" spacing={2} direction="row">
+        <Button size="small" variant="contained">Submit</Button>
+        <Button size="small" variant="text">Cancel</Button>
+      </Stack>
+    </div>
+
   );
 }
 
